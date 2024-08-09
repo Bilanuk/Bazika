ac67e9ce94294d821cc8f68e14d542494191c6cc
BiLaNuK <40918746+Bilanuk@users.noreply.github.com>
2024-08-09 21:48:14 +0300

    add nginx (#6)

--- terraform/task-definition.tf
+++ terraform/task-definition.tf
@@ -1,8 +1,8 @@
 resource "aws_ecs_task_definition" "bazika-backend" {
-  family             = "bazika-backend"
+  family             = "bazika-core"
   task_role_arn      = aws_iam_role.ecs_task_role.arn
   execution_role_arn = aws_iam_role.ecs_task_role.arn
-  network_mode       = "host"
+  network_mode       = "bridge"
   cpu                = 900
   memory             = 700
 
@@ -13,8 +13,8 @@ resource "aws_ecs_task_definition" "bazika-backend" {
 
   container_definitions = jsonencode([
     {
-      name      = "client"
-      image     = "${module.ecr.bazika-frontend-repository-url}:latest"
+      name      = "ingress"
+      image     = "${module.ecr.bazika-ingress-repository-url}:latest"
       essential = true
       memory    = 100
       cpu       = 20
@@ -23,11 +23,11 @@ resource "aws_ecs_task_definition" "bazika-backend" {
         {
           containerPort = 80
           hostPort      = 80
-          name          = "client"
+          name          = "ingress"
         }, {
           containerPort = 443
           hostPort      = 443
-          name          = "client-ssl"
+          name          = "ingress-ssl"
         }
       ]
 
@@ -39,9 +39,26 @@ resource "aws_ecs_task_definition" "bazika-backend" {
         }
       ]
 
+      environment = [
+        {
+          name  = "DOMAIN_NAME"
+          value = var.DOMAIN_NAME
+        }, {
+          name  = "API_URL"
+          value = var.API_URL
+        }, {
+          name  = "APP_URL"
+          value = var.APP_URL
+        }
+      ]
+
       dependsOn = [
         {
-          containerName = "core"
+          containerName = "backend"
+          condition     = "START"
+        },
+        {
+          containerName = "frontend"
           condition     = "START"
         }
       ]
@@ -51,7 +68,38 @@ resource "aws_ecs_task_definition" "bazika-backend" {
         options   = {
           "awslogs-group"         = aws_cloudwatch_log_group.bazika.name
           "awslogs-region"        = data.aws_region.current.name
-          "awslogs-stream-prefix" = "bazika-client"
+          "awslogs-stream-prefix" = "bazika-ingress"
+        }
+      }
+    },
+    {
+      name      = "frontend"
+      image     = "${module.ecr.bazika-frontend-repository-url}:latest"
+      essential = true
+      memory    = 100
+      cpu       = 20
+
+      portMappings = [
+        {
+          containerPort = 4000
+          hostPort      = 4000
+          name          = "nextjs"
+        }
+      ]
+
+      dependsOn = [
+        {
+          containerName = "backend"
+          condition     = "START"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options   = {
+          "awslogs-group"         = aws_cloudwatch_log_group.bazika.name
+          "awslogs-region"        = data.aws_region.current.name
+          "awslogs-stream-prefix" = "bazika-frontend"
         }
       }
     }, {
@@ -60,17 +108,11 @@ resource "aws_ecs_task_definition" "bazika-backend" {
       essential    = true
       memory       = 256
       cpu          = 256
+
       portMappings = [
         {
-          containerPort = 3000
-          hostPort      = 3000
-        }
-      ]
-
-      dependsOn = [
-        {
-          containerName = "postgres"
-          condition     = "START"
+          containerPort = 4001
+          hostPort      = 4001
         }
       ]
 
