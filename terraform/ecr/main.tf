59271ec47c8bc803c6042b4a8d9ff0f9ae552779
roma <bilanuk.bill@gmail.com>
2024-08-09 21:47:49 +0300

    add nginx

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -10,10 +10,20 @@ resource "aws_ecr_repository" "bazika-backend" {
   force_delete         = false
 }
 
+resource "aws_ecr_repository" "bazika-ingress" {
+  name = "bazika-ingress"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = false
+}
+
 output "bazika-frontend-repository-url" {
   value = aws_ecr_repository.bazika-frontend.repository_url
 }
 
 output "bazika-backend-repository-url" {
   value = aws_ecr_repository.bazika-backend.repository_url
+}
+
+output "bazika-ingress-repository-url" {
+  value = aws_ecr_repository.bazika-ingress.repository_url
 }
